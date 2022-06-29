7e82582b8bbe248246bafde599879a5f8a96ca87
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-06-29 11:11:17 +0000

    Add db.json to hold players information and modified the index.js and index.html accordingly

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("create_players_name");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        handlingServe(e.target.player_name.value);
+        form.reset();
+    })
+
+    function handlingServe(e){
+        fetch("http://localhost:3000/players", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                name: e
+            })
+        })
+        fetch("http://localhost:3000/players")
+        .then((resp) => resp.json())
+        .then((json) => renderPlayers(json));
+    
 
 
+        function renderPlayers(players) {
+            const info = document.getElementById('player_info');
+            players.forEach(player => {
+                const h2 = document.createElement('h2');
+                h2.innerHTML = player.name;
+                info.appendChild(h2);
+            });
+        }
+    }
+})
